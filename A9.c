9f175f6a6fdedd49eb8a99e313f5a8a5cc873973
abhirajghorpade <abhirajghorpade2007@gmail.com>
2026-04-02 10:54:29 +0530

    Create A9.c

---
+++ A9.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, originalNum, remainder, n = 0;
+    float result = 0.0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    // Store the number of digits of num in n
+    for (originalNum = num; originalNum != 0; ++n) {
+        originalNum /= 10;
+    }
+
+    originalNum = num;
+
+    // Calculate the sum of nth power of individual digits
+    while (originalNum != 0) {
+        remainder = originalNum % 10;
+        result += pow(remainder, n);
+        originalNum /= 10;
+    }
+
+    // Check if the result is equal to the original number
+    if ((int)result == num)
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
+}
